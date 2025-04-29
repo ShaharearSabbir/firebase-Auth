a913d533321d49c1eedaad245ade21885786475c
Shaharear Rahman Sabbir <imshaharear@gmail.com>
2025-04-29 14:02:51 +0600

    Update firebase.init.js

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAqHbkaH_X8AFkEhKczVeLBy-pxTrRkzZM",
+  apiKey: "",
   authDomain: "explore-firebase-af084.firebaseapp.com",
   projectId: "explore-firebase-af084",
   storageBucket: "explore-firebase-af084.firebasestorage.app",
-  messagingSenderId: "212779744424",
-  appId: "1:212779744424:web:0fb8c346f41749e8ee569d",
+  messagingSenderId: "",
+  appId: "",
 };
 
 // Initialize Firebase
